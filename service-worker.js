b339a2506efcd88074d22041bcfd643f471439a8
oosawa <tsu9co@gmail.com>
2025-03-14 14:13:04 +0900

    PWAアプリとして動作するように

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "hoikubag-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cache) => cache !== CACHE_NAME).map((cache) => caches.delete(cache))
+      );
+    })
+  );
+});
